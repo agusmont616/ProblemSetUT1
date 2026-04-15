4b2f082be84a9123fc1461c162bebdbcc54fa864
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2026-04-15 11:25:05 -0300

    Ejercicio 26 comentarios agregados

--- src/main/java/ucu/edu/aed/tda/entregables/ej26/Expresion.java
+++ src/main/java/ucu/edu/aed/tda/entregables/ej26/Expresion.java
@@ -6,23 +6,23 @@ public class Expresion {
 
     public boolean controlCorchetes(char[] lista) {
 
-        Stack<Character> pila = new Stack<>();
+        Stack<Character> pila = new Stack<>(); // Creamos la pila para almacenar los corchetes
 
-        for (int i = 0; i < lista.length; i++) {
+        for (int i = 0; i < lista.length; i++) {// Se recorre la lista de caracteres
 
             if (lista[i] == '{') {
-                pila.push(lista[i]);
+                pila.push(lista[i]); // Si el corchete es abierto, se agrega a la pila
             } 
-            else if (lista[i] == '}') {
+            else if (lista[i] == '}') { //Si es cerrado, se verifica si está vacía
 
                 if (pila.isEmpty()) {
-                    return false;
+                    return false; // Si está vacía, retorna falso
                 }
 
-                pila.pop();
+                pila.pop(); // Si no, se saca el último corchete de la pila
             }
         }
 
-        return pila.isEmpty();
+        return pila.isEmpty(); // Al finalizar el recorrido, si la pila está vacía, devuelve true (balanceado), de lo contrario devuelve false.
     }
 }
